dbd120515776c16b8bcfe204acfbbf7e0fd6e239
gtursunxujayev-art <gtursunxujayev@gmail.com>
2025-10-15 00:41:19 +0500

    Update prisma.ts

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,12 +1,9 @@
 import { PrismaClient } from '@prisma/client'
 
-// Prevent creating many PrismaClient instances in dev/hot-reload
 const globalForPrisma = global as unknown as { prisma?: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['warn', 'error'],
-  })
+  new PrismaClient({ log: ['warn', 'error'] })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
