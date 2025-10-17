e7dd61a11bcd47f9f8cce35f36146d1b59190e06
gtursunxujayev-art <gtursunxujayev@gmail.com>
2025-10-17 20:33:01 +0500

    Delete app/(auth) directory

--- app/(auth)/auth-layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function AuthShell({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-[70vh] w-full flex items-center justify-center px-4 pt-6">
-      <div className="w-full max-w-sm rounded-2xl border border-zinc-800 bg-zinc-900/40 p-6 shadow-lg">
-        {children}
-      </div>
-    </div>
-  );
-}
